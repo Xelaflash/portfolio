8ad41b174c7af0f47c165f01f9dd0729af57d89f
Alexandre Gros <xelaflash@gmail.com>
2020-09-21 12:35:07 -0400

    Automated commit at 2020-09-21 16:35:07 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/confirm-30833a58.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var swalButton = document.getElementById('confirm');
+
+var elem = document.createElement("div");
+elem.innerHTML = 'You take the <strong id="blue-pill">blue pill</strong> the story ends. <br> You take the <strong id="red-pill">red pill</strong> you stay in Wonderland, and I show you how deep the rabbit hole goes.';
+
+function bindSweetAlertButton(e) {
+  e.preventDefault();
+  swal({
+    title: "Red or Blue pill?",
+    content: elem,
+    icon: 'https://res.cloudinary.com/dcf4y5ngp/image/upload/f_auto,q_auto/v1543430920/morpheus2.png',
+    buttons: ["Blue pill", "Red pill"],
+    dangerMode: true
+  }).then(function (isConfirm) {
+    if (isConfirm) {
+      swal("There is no turning back! You are going to enter the Matrix!", {
+        icon: "warning"
+      }).then(function () {
+        window.location.href = "/easter-matrix.html";
+      });
+    } else {
+      swal("You chose to stay in your illusion reality!", {
+        icon: "error"
+      });
+    }
+  });
+};
+
+if (swalButton) {
+  swalButton.addEventListener('click', bindSweetAlertButton);
+};
